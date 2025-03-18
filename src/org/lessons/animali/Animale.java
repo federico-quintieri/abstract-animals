c995800918c308f5d23ecc59a787f8dc7e2a47e9
Federico Quintieri <fedequinti@gmail.com>
2025-03-18 21:07:02 +0100

    first commit

---
+++ src/org/lessons/animali/Animale.java
@@ -0,0 +1,14 @@
+package org.lessons.animali;
+
+public abstract class Animale {
+
+    // Metodo concreto dormi per tutti gli animali
+    public void dormi() {
+        System.out.println("Zzz");
+    }
+
+    // Metodo astratto verso 
+    public abstract void verso();
+    // Metodo astratto mangia
+    public abstract void mangia();
+}
---
+++ src/org/lessons/animali/Passerotto.java
@@ -0,0 +1,5 @@
+package org.lessons.animali;
+
+public class Passerotto {
+
+}
